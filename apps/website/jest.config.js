d0297bce9dadf4b4f52ca74b2221e35202311781
Michael Oddis <856914+michaeloddis@users.noreply.github.com>
2020-12-18 09:54:47 -0500

    intial commit

--- apps/website/jest.config.js
+++ apps/website/jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  name: 'website',
-  preset: '../../jest.config.js',
+  preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.[tj]sx?$': 'ts-jest'
+    '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/apps/website'
+  coverageDirectory: '../../coverage/apps/website',
+  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
+  displayName: 'website',
 };
